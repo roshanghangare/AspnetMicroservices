4c3be198e4ff4facd4531b1bd2edc0068d4839e2
Roshan Ghangare <roshanghangare@gmail.com>
2022-03-20 10:33:58 +0530

    36. Connect Redis Docker Container from Basket.API Microservice w/ AddStackExchange

--- Source/Services/Basket/Basket.Api/Startup.cs
+++ Source/Services/Basket/Basket.Api/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.Api
 {
@@ -25,6 +19,8 @@ namespace Basket.Api
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddStackExchangeRedisCache(options => 
+				options.Configuration = "CacheSettings:ConnectionString");
 
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
