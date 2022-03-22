eea6bd2ecfc22fa506add4b63b4e67e8746a70da
Roshan Ghangare <roshanghangare@gmail.com>
2022-03-22 07:19:13 +0530

    53. Create DiscountController Class for Discount.API Microservice

---
+++ Source/Services/Discount/Discount.Api/Controllers/DiscountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Discount.Api.Entities;
+using Discount.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.Api.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class DiscountController : ControllerBase
+	{
+		readonly IDiscountRepository repository;
+
+		public DiscountController(IDiscountRepository repository)
+		{
+			this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+		}
+
+		[HttpGet("{productName}", Name = "GetDiscount")]
+		[ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+		{
+			return Ok(await repository.GetDiscount(productName));
+		}
+
+		[HttpPost] 
+		[ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+		{
+			await repository.CreateDiscount(coupon);
+			return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+		}
+
+		[HttpPut]
+		[ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+		{
+			return Ok(await repository.UpdateDiscount(coupon));
+		}
+
+		[HttpDelete("{productName}", Name = "DeleteDiscount")]
+		[ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+		{
+			return Ok(await repository.DeleteDiscount(productName));
+		}
+	}
+}
--- Source/Services/Discount/Discount.Api/Startup.cs
+++ Source/Services/Discount/Discount.Api/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Discount.Api.Repositories;
 
 namespace Discount.Api
 {
@@ -25,6 +26,7 @@ namespace Discount.Api
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
