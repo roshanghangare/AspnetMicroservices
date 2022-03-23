4c74842534ae269b919db17bd6203dfc5398f7f9
Roshan Ghangare <roshanghangare@gmail.com>
2022-03-23 07:46:17 +0530

    64. Developing discount.proto ProtoBuf file for Exposing Crud Services Discount Grpc

--- Source/Services/Discount/Discount.Grpc/Startup.cs
+++ Source/Services/Discount/Discount.Grpc/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Discount.Grpc.Services;
+//using Discount.Grpc.Services;
 
 namespace Discount.Grpc
 {
@@ -30,7 +30,7 @@ namespace Discount.Grpc
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapGrpcService<GreeterService>();
+				//endpoints.MapGrpcService<GreeterService>();
 
 				endpoints.MapGet("/", async context =>
 					{
