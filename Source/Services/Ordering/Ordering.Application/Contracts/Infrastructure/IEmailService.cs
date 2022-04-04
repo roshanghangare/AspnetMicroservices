02808255cbde1d95fb7a0d4f2c1a061265cbdef7
Roshan Ghangare <roshanghangare@gmail.com>
2022-04-04 20:21:51 +0530

    92. Developing Ordering.Application Layer - Application Contracts

---
+++ Source/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Ordering.Application.Models;
+
+namespace Ordering.Application.Contracts.Infrastructure
+{
+	interface IEmailService
+	{
+		Task<bool> SendEmail(Email email);
+	}
+}
---
+++ Source/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+	interface IOrderRepository : IAsyncRepository<Order>
+	{
+		Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+	}
+}
