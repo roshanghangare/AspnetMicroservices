40ae465fac7e9fdce62ac331a7eac1a1cb0e032a
Roshan Ghangare <roshanghangare@gmail.com>
2022-04-05 07:13:11 +0530

    95. Developing Ordering.Application Layer - Application Command Features - Checkout

--- Source/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs
+++ Source/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -3,7 +3,7 @@ using Ordering.Application.Models;
 
 namespace Ordering.Application.Contracts.Infrastructure
 {
-	interface IEmailService
+	public interface IEmailService
 	{
 		Task<bool> SendEmail(Email email);
 	}
